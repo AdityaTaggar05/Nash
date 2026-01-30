235e7b21613147bd9cc4413d6560a10a0a194ed3
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-30 18:28:05 +0530

    Added basic server template

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+//import authRoutes from "./modules/auth/auth.routes.js";
+//import roomRoutes from "./modules/chat-room/chat-room.routes.js";
+
+const app = express();
+
+// Middleware initialisation
+app.use(express.json());
+
+app.get("/health", (_req: Request, res: Response) => {
+  res.status(204).json({});
+});
+
+//app.use("/auth", authRoutes);
+//app.use("/room", roomRoutes);
+
+export default app;
