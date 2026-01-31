7e9d735e26243ba99a00cc46463bcfbd5054434a
Vedansh-Sethi <vedansh25sethi@gmail.com>
2026-01-31 16:34:14 +0530

    [UI] : Added interactivity in groups list

--- app/lib/pages/groups/widgets/group_tab_card.dart
+++ app/lib/pages/groups/widgets/group_tab_card.dart
@@ -1,5 +1,6 @@
 import 'package:app/config/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class GroupTabCard extends StatelessWidget {
   final String groupName;
@@ -20,50 +21,55 @@ class GroupTabCard extends StatelessWidget {
         color: context.colorScheme.surface,
         borderRadius: BorderRadius.circular(16.0),
       ),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 32,
-            backgroundImage: AssetImage('assets/pfp.png'),
-          ),
-          SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  groupName,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: context.colorScheme.onSurface,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 2.0,),
-                Text(
-                  groupDescription,
-                  style: TextStyle(
-                    fontSize: 10,
-                    color: Colors.grey.shade700,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 8),
-                Text(
-                  '$memberNumber members',
-                  style: TextStyle(
-                    color: context.colorScheme.onSurfaceVariant,
-                    fontSize: 12
-                  )
-                )
-              ],
+      child: GestureDetector(
+        onTap: () {
+            context.push('/groups/:group_id');
+        },
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 32,
+              backgroundImage: AssetImage('assets/pfp.png'),
             ),
-          ),
-        ],
+            SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    groupName,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: context.colorScheme.onSurface,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 2.0,),
+                  Text(
+                    groupDescription,
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.grey.shade700,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '$memberNumber members',
+                    style: TextStyle(
+                      color: context.colorScheme.onSurfaceVariant,
+                      fontSize: 12
+                    )
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
