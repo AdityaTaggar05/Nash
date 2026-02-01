65c8e2661ac9707df42ffd3cae480bb334f9b53d
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-02-01 10:02:52 +0530

    [fix] Error response code in server

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -16,7 +16,7 @@ export const getUser = async (req: AuthRequest, res: Response) => {
     const result = await userServices.getUser(req.user, req.body);
     res.status(201).json(result);
   } catch (err: any) {
-    res.status(404).json({ error: err.message });
+    res.status(400).json({ error: err.message });
   }
 };
 
